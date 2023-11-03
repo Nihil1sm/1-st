fb7b0f1a77a63d5fb5290058b49def6ab0e2e778
DANIK <shchetinin0374@gmail.com>
2023-11-04 01:54:46 +0300

    1-st
    so how are you?

---
+++ 1-st.py
@@ -0,0 +1,13 @@
+user=int(input('how are u from 1 to 10?\n'))
+
+num = 5
+num2=7
+if 1 <= user <= num:
+    print("u'r unhappy unlucky((")
+elif num <=user <= num2:
+    print("ohh u'r middle od happiness not bad")
+elif user<=10:
+    print("U'r happy congrats")
+else:
+    print("wtf? i told u from 1 to 10! NO MORE! NO LESS! IDIOT")
+
